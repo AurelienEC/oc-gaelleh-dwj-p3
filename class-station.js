a0c481239ae3bb684fa66ee558b1dbe597caddb4
GaelleHenaf <gaelle.henaf@gmail.com>
2017-02-13 18:29:13 +0100

    class station
    declaration de la class station

---
+++ class-station.js
@@ -0,0 +1,11 @@
+class Station {
+	var name = '';
+	var address = '';
+	var banking = false;
+	var bonus = false;
+	var status = 'OPEN';
+	var position = '';
+	var bike-stand = 0;
+	var available-bike-stand = 0;
+	var available-stand = 0;
+}
