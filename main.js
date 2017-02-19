283ae2257e37029bdd19c21913461453d55fda46
GaelleHenaf <gaelle.henaf@gmail.com>
2017-02-19 21:36:27 +0100

    markercluster
    toutes les stations affichées avec le système de markercluster

--- main.js
+++ main.js
@@ -46,38 +46,11 @@ var stations = []; // charger stations -> faire un appel puis boucle pour creer 
 
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 48.856578, lng: 2.351828},
     zoom: 12
   });
-  var markers = setMarkers(map);
-
-}
-
-function setMarkers(map) {
-  // Adds markers to the map.
-
-  // Marker sizes are expressed as a Size of X,Y where the origin of the image
-  // (0,0) is located in the top left of the image.
-
-  // Origins, anchor positions and coordinates of the marker increase in the X
-  // direction to the right and in the Y direction down.
-  // var image = {
-  //   //url: 'images/beachflag.png',
-  //   // This marker is 20 pixels wide by 32 pixels high.
-  //   size: new google.maps.Size(20, 32),
-  //   // The origin for this image is (0, 0).
-  //   origin: new google.maps.Point(0, 0),
-  //   // The anchor for this image is the base of the flagpole at (0, 32).
-  //   anchor: new google.maps.Point(0, 32)
-  // };
-  // Shapes define the clickable region of the icon. The type defines an HTML
-  // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-  // The final coordinate closes the poly by connecting to the first coordinate.
-  // var shape = {
-  //   coords: [1, 1, 1, 20, 18, 20, 18, 1],
-  //   type: 'poly'
-  // };
+  var markers = [];
   for (var i = 0; i < stations.length; i++) {
     var station = stations[i];
     var marker = new google.maps.Marker({
@@ -88,5 +61,10 @@ function setMarkers(map) {
       title: station.name
       //zIndex: beach[3]
     });
+    markers.push(marker);
   }
+  var markerCluster = new MarkerClusterer(map, markers,
+    {imagePath: 'images/m'});
+      
 }
+
