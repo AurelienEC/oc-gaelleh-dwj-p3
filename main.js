887d180c8cf2a5f0705f6ae14f72b685df30f79a
GaelleHenaf <gaelle.henaf@gmail.com>
2017-03-12 20:50:27 +0100

    fini ?

--- main.js
+++ main.js
@@ -8,35 +8,30 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 48.856578, lng: 2.351828},
     zoom: 11,
-
   });
   var stations = Object.create(StationsObject);
   stations.init(map);
 
-   infoBulle = new google.maps.InfoWindow({
-                content: "loading..."
-            });
-
-
+  infoBulle = new google.maps.InfoWindow({
+    content: "loading..."
+  });
 }
 
-// OPEN DATA PARIS 
-
 var Station = {
-  // Initialise la station
   init: function (number, name, address, banking, bonus, status, position, bikeStand, availableBikeStand, availableBike, marker) {
     this.number = number;
-      this.name = name;
-      this.address = address;
-      this.banking = banking;
-      this.bonus = bonus;
-      this.status = status;
-      this.position = position;
-      this.bikeStand =  bikeStand;
-      this.availableBikeStand = availableBikeStand;
-      this.availableBike = availableBike;
-      this.contentInfoBulle = 
-      '<h2>'+this.name+'</h2>';
+    this.name = name;
+    this.address = address;
+    this.banking = banking;
+    this.bonus = bonus;
+    this.status = status;
+    this.position = position;
+    this.bikeStand =  bikeStand;
+    this.availableBikeStand = availableBikeStand;
+    this.availableBike = availableBike;
+    this.reserve = false;
+    this.contentInfoBulle = 
+    '<h2>'+this.name+'</h2>';
 
       // créer le marker
       this.marker = new google.maps.Marker({
@@ -49,7 +44,10 @@ var Station = {
       });
 
       // choisir l'icon
-      if (this.status == 'CLOSED') {
+      if (this.reserve == true) {
+        this.marker.icon = 'images/closed.png';
+      }
+      else if (this.status == 'CLOSED') {
         this.marker.icon = 'images/closed.png';
       }
       else if(this.availableBike == 0) {
@@ -62,61 +60,60 @@ var Station = {
         this.marker.icon = 'images/open.png';     
       }
       google.maps.event.addListener(this.marker, "click",  function() {
+        $('html,body').animate({scrollTop: $("#map").offset().top}, 'slow'      );
         $('#station-infos').css('display', 'block');
         infoBulle.setContent(this.html);
         infoBulle.open(map, this);
         $('.info').attr('id', this.station.number);
         if (this.status == 'CLOSED') {
-           $('#statut').text('station fermée');
-        }
-        else {
-           $('#statut').text('station ouverte');
-        }
-        $('#name').text(this.station.name);
-        $('#address').text(this.station.address);
-        $('#velosDispo').text(this.station.availableBike);
-        $('#placesDispo').text(this.station.availableBikeStand);
-        $('#placesTotales').text(this.station.bikeStand);
-        if (this.station.banking == 'True') {
-          $('#paiement').text('Oui');
-        }
-        else {
-          $('#paiement').text('Non');
-        }
-        if (this.station.bonus == 'True') {
-          $('#bonus').text('Oui');
-        }
-        else {
-          $('#bonus').text('Non');
-        }
-        if (this.station.availableBike == 0) {
-          $('#bouton-reservation').css('display', 'none');
-        }
-        else {
-          $('#bouton-reservation').css('display', 'block');
-          $('#div-signature').css('display', 'none');
-          $('#bouton-reservation').text('Cliquer ici pour réserver un vélo');
-        }
-      });
+         $('#statut').text('station fermée');
+       }
+       else {
+         $('#statut').text('station ouverte');
+       }
+       $('#name').text(this.station.name);
+       $('#address').text(this.station.address);
+       $('#velosDispo').text(this.station.availableBike);
+       $('#placesDispo').text(this.station.availableBikeStand);
+       $('#placesTotales').text(this.station.bikeStand);
+       if (this.station.banking == 'True') {
+        $('#paiement').text('Oui');
+      }
+      else {
+        $('#paiement').text('Non');
+      }
+      if (this.station.bonus == 'True') {
+        $('#bonus').text('Oui');
+      }
+      else {
+        $('#bonus').text('Non');
+      }
+      if (this.station.availableBike == 0) {
+        $('#bouton-reservation').css('display', 'none');
+      }
+      else {
+        $('#bouton-reservation').css('display', 'block');
+        $('#div-signature').css('display', 'none');
+        $('#bouton-reservation').text('Cliquer ici pour réserver un vélo');
+      }
+      $('#guide-station').css('display', "none");
+    });
 
   },
 };
 
 
-
-
-
-
 var StationsObject =  {
-// charger stations -> faire un appel puis boucle pour creer chaque stations
   init: function (map) {
     this.stationsArray = [];
     this.markers = [];
     this.infoBulles = [];
-
+    this.stationReserve = '';
 
     this.stationsArray = this.getStations(map);
-    this.createMarkers(this.stationsArray);
+    this.stationReserve = Object.create(Reservation);
+    this.stationReserve.init();
+    this.createMarkers(this.stationsArray, this.stationReserve.idStation);
   },
 
 
@@ -132,54 +129,109 @@ var StationsObject =  {
 
         for (var i = 0; i < data.records.length; i++) {
 
-            var station = data.records[i].fields;
-            stationNew = Object.create(Station);
-            stationNew.init(station.number, station.name, station.address, station.banking, station.bonus, station.status, station.position, station.bike_stands, station.available_bike_stands, station.available_bikes);
-            markerCluster.addMarker(stationNew.marker);
-            array.push(stationNew);
+          var station = data.records[i].fields;
+          stationNew = Object.create(Station);
+          stationNew.init(station.number, station.name, station.address, station.banking, station.bonus, station.status, station.position, station.bike_stands, station.available_bike_stands, station.available_bikes);
+          markerCluster.addMarker(stationNew.marker);
+          array.push(stationNew);
         }
       }
-  })
-  return array;    
-},
+    })
+    return array;    
+  },
 
-  createMarkers: function (stations) {
-     for (var i = 0; i < stations.length; i++) {
+  createMarkers: function (stations, idStationReserve) {
+   for (var i = 0; i < stations.length; i++) {
       station = stations[i];
+      if (idStationReserve != 0) {
+        station.reserve = true;
+      }
       this.markers.push(station.marker);
     }
   }
 
-  
 };
 
-// CANVAS
 
-var signaturePad = new SignaturePad(document.getElementById('signature-pad'), {
-  backgroundColor: 'rgba(255, 255, 255, 0)',
-  penColor: 'rgb(0, 0, 0)'
-});
-var boutonReserver = document.getElementById('reserver');
-var boutonEffacer= document.getElementById('effacer');
 
-boutonReserver.addEventListener('click', function (event) {
-  var stationNumber = $('.info').attr('id');
-  var nomStation = $('#name').text();
-  if (sessionStorage.length == 0) {
-    stockageReservation(stationNumber, nomStation);
+var Reservation = {
+
+  // Initialise la station
+  init: function () {
+    this.idStation = sessionStorage.getItem('idStation');
+    this.nomStation = sessionStorage.getItem('nomStation');
+    this.datetimeReservation = sessionStorage.getItem('datetimeReservation');
+    this.tempsPasse = new Date().getTime() -  this.datetimeReservation;
+    this.reservationForm();
+    this.checkTime();
+    this.checkReservation();
+  },
+
+  checkTime: function() {
+    if (this.tempsPasse > 20 * 60 * 1000) {
+     sessionStorage.clear();
+    }
+  },
+
+  checkReservation: function() {
+    if (sessionStorage.length == 0) {
+      $('#reservationEnCours').text('Aucune réservation en cours');
+    }
+    else {
+      setInterval(this.affichageReservation, 1000);
+
+    }
+  },
+
+  affichageReservation: function() {
+
+    var tempsPasse = new Date().getTime() -  sessionStorage.getItem('datetimeReservation');
+    var tempsRestant = 20 * 60 * 1000 - tempsPasse;
+    var minutes = Math.floor(tempsRestant/60000);
+    var secondes = Math.floor((tempsRestant - minutes * 60 * 1000)/1000);
+    $('#reservationEnCours').text('Vélo réservé station ' + sessionStorage.getItem('nomStation') + ' pour ' + minutes + 'min et ' + secondes + 's');
+
+
+  },
+
+  reservationForm: function() {
+    var signaturePad = new SignaturePad(document.getElementById('signature-pad'), {
+      backgroundColor: 'rgba(255, 255, 255, 0)',
+      penColor: 'rgb(0, 0, 0)'
+    });
+
+    $('#reserver').on('click', function (event) {
+      var stationNumber = $('.info').attr('id');
+      var nomStation = $('#name').text();
+      if (signaturePad.isEmpty() == false ) {
+
+        if (sessionStorage.length == 0) {
+          sessionStorage.setItem('idStation', stationNumber);
+          sessionStorage.setItem('datetimeReservation', new Date().getTime());
+          sessionStorage.setItem('nomStation', nomStation);
+          this.affichageReservation();
+        }
+        else {
+          sessionStorage.setItem('idStation', stationNumber);
+          sessionStorage.setItem('datetimeReservation', new Date().getTime());
+          sessionStorage.setItem('nomStation', nomStation);
+          this.affichageReservation();
+          alert('Votre nouvelle réservation a remplacé l\'ancienne');
+        }
+
+
+      }
+      else {
+        alert('Vous n\'avez pas signé');
+      }
+
+    });
+
+    $('#effacer').on('click', function (event) {
+      signaturePad.clear();
+    }); 
   }
-  else {
-     stockageReservation(stationNumber, nomStation);
-     alert('Votre nouvelle réservation a remplacé l\'ancienne');
-  }
-});
-
-boutonEffacer.addEventListener('click', function (event) {
-  signaturePad.clear();
-});
-
-
-// RESERVATION
+};
 
 $('#bouton-reservation').click(function() {
   $('#div-signature').toggle();
@@ -192,72 +244,39 @@ $('#bouton-reservation').click(function() {
   }
 });
 
-
-// STORAGE
-
-function stockageReservation (stationNumber, nomStation) {
-  sessionStorage.setItem('idStation', stationNumber);
-  sessionStorage.setItem('datetimeReservation', new Date().getTime())
-  sessionStorage.setItem('nomStation', nomStation)
-  console.log(sessionStorage.getItem('datetimeReservation'));
-}
-var tempsPasse = new Date().getTime() - sessionStorage.getItem('datetimeReservation');
-if (tempsPasse > 20 * 60 * 1000) {
-   sessionStorage.clear();
-}
-if (sessionStorage.length == 0) {
-  $('#reservationEnCours').text('Aucune réservation en cours');
-}
-else {
-  var tempsRestant = 20 * 60 * 1000 - tempsPasse;
-  var minutes = Math.floor(tempsRestant/60000);
-  var secondes = Math.floor((tempsRestant - minutes * 60 * 1000)/1000);
-    //console.log(tempsRestant.getSeconds());
-  $('#reservationEnCours').text('Vous avez un vélo de réservé à la station ' + sessionStorage.getItem('nomStation') + ' pour ' + minutes + 'min et ' + secondes + 's');
-}
-
-
 // SLIDER
 
-for (var i = 1; i <= 5; i++) {
-  $('#slider ul').append('<li class="li' + i + '" ><img src="images/slide' + i + '.png"></li>' );
+var images = [
+"images/slide1.png", 
+"images/slide2.png", 
+"images/slide3.png", 
+"images/slide4.png", 
+"images/slide5.png"
+];
+var num = 0;
+
+function next() {
+  var slider = document.getElementById("slider");
+  num++;
+  if ( num >= images.length ) {
+    num = 0;
+  }
+  slider.src = images[num];
 }
 
-$(function(){
-  function next(){
-    if(parseInt($("#slider ").css("margin-left"))>-(2000-$(window).width())){ 
-      $("#slider").animate({marginLeft: "-="+250+"px"}, 500);
-    } 
-    else {
-      $("#slider ").animate({marginLeft: "0px"}, 500);
-    }
+function prev() {
+  var slider = document.getElementById("slider");
+  num--;
+  if ( num < 0 ) {
+    num = images.length-1;
   }
+  slider.src = images[num];
+}
 
-  
-  function back(){
-    if(parseInt($("#slider ").css("margin-left"))<0){
-      $("#slider ").animate({marginLeft: "+="+250+"px"}, 500 );
-    } 
-    else {
-      $("#slider ").animate({marginLeft: "0"}, 500);
-    }
-  }
-  
-  $(".suivant").click(function(){
-    next();
-  });
-  
-  $(".precedent").click(function(){
-    back();
-  });
+$('#btnNext').click(function() {
+  next();
+});
 
-  document.addEventListener("keydown", function(e){
-    if(e.keyCode === 37){
-    back();
-    }
-    else if(e.keyCode === 39){
-    next();
-    }
-  });
-  
+$('#btnPrev').click(function() {
+  prev();
 });
